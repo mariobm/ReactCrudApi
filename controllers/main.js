265b793dd1ea57ebc3e0136930900fac6dbbb648
Mario <mariob@vivaldi.net>
2019-08-13 14:05:59 +0200

    Initialization

---
+++ controllers/main.js
@@ -0,0 +1,48 @@
+const getTableData = (req, res, db) => {
+    db.select('*').from('testtable1')
+      .then(items => {
+        if(items.length){
+          res.json(items)
+        } else {
+          res.json({dataExists: 'false'})
+        }
+      })
+      .catch(err => res.status(400).json({dbError: 'db error'}))
+  }
+  
+  const postTableData = (req, res, db) => {
+    const { name, category, price, stocked } = req.body
+    const added = new Date()
+    db('products').insert({name, category, price, stocked})
+      .returning('*')
+      .then(item => {
+        res.json(item)
+      })
+      .catch(err => res.status(400).json({dbError: 'db error'}))
+  }
+  
+  const putTableData = (req, res, db) => {
+    const { id, name, category, price, stocked } = req.body
+    db('products').where({id}).update({first, last, email, phone, location, hobby})
+      .returning('*')
+      .then(item => {
+        res.json(item)
+      })
+      .catch(err => res.status(400).json({dbError: 'db error'}))
+  }
+  
+  const deleteTableData = (req, res, db) => {
+    const { id } = req.body
+    db('products').where({id}).del()
+      .then(() => {
+        res.json({delete: 'true'})
+      })
+      .catch(err => res.status(400).json({dbError: 'db error'}))
+  }
+  
+  module.exports = {
+    getTableData,
+    postTableData,
+    putTableData,
+    deleteTableData
+  }
